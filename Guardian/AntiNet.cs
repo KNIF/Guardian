c6235b67c82c9ec17242a708818ae2d4ffc8ada8
KNIF <admin@knif.dev>
2019-09-14 20:33:42 +0200

    Removed Auth & Updated some protections
    Removed Auth traces in Main.cs
    Optimized AntiNet.cs

--- Guardian/AntiNet.cs
+++ Guardian/AntiNet.cs
@@ -1,26 +1,29 @@
 ﻿using System.Diagnostics;
 using System.IO;
+using System.Collections.Generic;
 
 namespace Guardian
 {
     internal class AntiNet : Main
     {
-        internal static void Initialize()
+        internal static void Initialize(List<string> blacklist)
         {
-            VerifyNet();
+            VerifyNet(blacklist);
         }
 
-        static void VerifyNet()
+        static void VerifyNet(List<string> blacklist)
         {
             try {
-                string text = File.ReadAllText("C:\\Windows\\System32\\Drivers\\etc\\hosts").ToLower();
-                if (text.Contains("crack"))
-                {
-                    MemberFilter(string.Concat(new object[]
-                    {
-                        "START CMD /C \"COLOR 4 && TITLE Guardian by KNIF#0001 && ECHO. && echo   ██████╗ ██╗   ██╗ █████╗ ██████╗ ██████╗ ██╗ █████╗ ███╗   ██╗ && echo  ██╔════╝ ██║   ██║██╔══██╗██╔══██╗██╔══██╗██║██╔══██╗████╗  ██║ && echo  ██║  ███╗██║   ██║███████║██████╔╝██║  ██║██║███████║██╔██╗ ██║ && echo  ██║   ██║██║   ██║██╔══██║██╔══██╗██║  ██║██║██╔══██║██║╚██╗██║ && echo  ╚██████╔╝╚██████╔╝██║  ██║██║  ██║██████╔╝██║██║  ██║██║ ╚████║ && echo   ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ && ECHO ----------------------------------------------------------------- && ECHO malicious hosts file detected! Please close and try again! && ECHO ----------------------------------------------------------------- && TIMEOUT 10\""
-                    }));
-                    Process.GetCurrentProcess().Kill();
+                string hosts = File.ReadAllText("C:\\Windows\\System32\\Drivers\\etc\\hosts").ToLower();
+
+                foreach (string s in blacklist) {
+                    if (hosts.Contains(s.ToLower())) {
+                        MemberFilter(string.Concat(new object[]
+                        {
+                            "START CMD /C \"COLOR 4 && TITLE Guardian by KNIF#0001 && ECHO. && echo   ██████╗ ██╗   ██╗ █████╗ ██████╗ ██████╗ ██╗ █████╗ ███╗   ██╗ && echo  ██╔════╝ ██║   ██║██╔══██╗██╔══██╗██╔══██╗██║██╔══██╗████╗  ██║ && echo  ██║  ███╗██║   ██║███████║██████╔╝██║  ██║██║███████║██╔██╗ ██║ && echo  ██║   ██║██║   ██║██╔══██║██╔══██╗██║  ██║██║██╔══██║██║╚██╗██║ && echo  ╚██████╔╝╚██████╔╝██║  ██║██║  ██║██████╔╝██║██║  ██║██║ ╚████║ && echo   ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ && ECHO ----------------------------------------------------------------- && ECHO malicious hosts file detected! Please close and try again! && ECHO ----------------------------------------------------------------- && TIMEOUT 10\""
+                        }));
+                        Process.GetCurrentProcess().Kill();
+                    }
                 }
             } catch { }
         }
--- Guardian/Main.cs
+++ Guardian/Main.cs
@@ -1,86 +1,25 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Net;
-using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace Guardian
 {
     public class Main
     {
-        public static void Start(string licenseKey, bool AntiDump, bool AntiDebug, bool AntiEmulation, bool AntiNet)
+        public static void Start(string urlForHashCheck, bool AntiDump, bool AntiDebug, bool AntiEmulation, List<string> AntiNetBlacklist)
         {
             WebRequest.DefaultWebProxy = new WebProxy();
 
-            bool active = false;
-            string ws = "";
-
-            try {
-                ws = new WebClient().DownloadString("https://guardian.dedev.io/?k=" + licenseKey);
-            }
-            catch (Exception ex) {
-                MessageBox.Show("Error while attempting to activate Guardian.\nThis application won't be protected by Guardian.\n\nPress OK to continue.", "Guardian by KNIF#0001", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                active = false;
-            }
-
-            if (ws == "YES") {
-                active = true;
-            }
-            else {
-                active = false;
-            }
-
-            if (active == true) {
-                if (AntiDump)
-                    Guardian.AntiDump.Initialize();
-                if (AntiDebug)
-                    Guardian.AntiDebug.Initialize();
-                if (AntiEmulation)
-                    Guardian.AntiEmulation.Initialize();
-                if (AntiNet)
-                    Guardian.AntiNet.Initialize();
-            }
-            else {
-                MessageBox.Show("Guardian is not licensed.\nYou need to enter a valid product key to use this tool.\nThis application won't be protected by Guardian.\nAdd KNIF#0001 on Discord to buy a license key.\n\nPress OK to continue.", "Guardian by KNIF#0001", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-        }
-
-        public static void Start(string licenseKey, string urlForHashCheck, bool AntiDump, bool AntiDebug, bool AntiEmulation, bool AntiNet)
-        {
-            WebRequest.DefaultWebProxy = new WebProxy();
-
-            bool active = false;
-            string ws = "";
-
-            try {
-                ws = new WebClient().DownloadString("https://guardian.dedev.io/?k=" + licenseKey);
-            }
-            catch (Exception ex) {
-                MessageBox.Show("Error while attempting to activate Guardian.\nThis application won't be protected by Guardian.\n\nPress OK to continue.", "Guardian by KNIF#0001", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                active = false;
-            }
-
-            if (ws == "YES") {
-                active = true;
-            }
-            else {
-                active = false;
-            }
-
-            if (active == true) {
-                if (!string.IsNullOrEmpty(urlForHashCheck))
-                    Guardian.AntiTamper.Initialize(urlForHashCheck);
-                if (AntiDump)
-                    Guardian.AntiDump.Initialize();
-                if (AntiDebug)
-                    Guardian.AntiDebug.Initialize();
-                if (AntiEmulation)
-                    Guardian.AntiEmulation.Initialize();
-                if (AntiNet)
-                    Guardian.AntiNet.Initialize();
-            }
-            else {
-                MessageBox.Show("Guardian is not licensed.\nYou need to enter a valid product key to use this tool.\nThis application won't be protected by Guardian.\nAdd KNIF#0001 on Discord to buy a license key.\n\nPress OK to continue.", "Guardian by KNIF#0001", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
+            if (!string.IsNullOrEmpty(urlForHashCheck))
+                Guardian.AntiTamper.Initialize(urlForHashCheck);
+            if (AntiDump)
+                Guardian.AntiDump.Initialize();
+            if (AntiDebug)
+                Guardian.AntiDebug.Initialize();
+            if (AntiEmulation)
+                Guardian.AntiEmulation.Initialize();
+            if (AntiNetBlacklist.Count > 0)
+                Guardian.AntiNet.Initialize(AntiNetBlacklist);
         }
 
         internal static void MemberFilter(string A_0)
